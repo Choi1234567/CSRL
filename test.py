6e7f2f456b9375bf81b925856a2406bceeea91e5
liuji <liuji@yingmi.cn>
2022-08-02 10:47:26 +0800

    test

--- test.py
+++ test.py
@@ -1,7 +1,4 @@
-import torch
+import netron
 
-device = torch.device(f'cuda:{int(0)}' if torch.cuda.is_available() else 'cpu')
-image=torch.load('/Users/liuji/PycharmProjects/CSRL/environments_and_constraints/lunar_lander/trained_agent_files'
-                 '/p0_train.pt', map_location=device)
 
-print(image)
+netron.start('./environments_and_constraints/lunar_lander/trained_agent_files/p0_train.pt')
